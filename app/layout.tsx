cdc06b2deffbca104fbf2097d91ccb53f7b1b80d
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2023-08-07 08:56:36 -0300

    Fixed Bug Animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,4 +37,3 @@ export default function RootLayout({
 // Boton de ChangeTheme con FramerMotion -> Agregar Funcionaldiad (MVP)
 // Modificar titulo de la pagina en cada pagina (tomar estado anterior) -> Pestaña del navegador
 // Responsively
-// Fix Accordion Animation
